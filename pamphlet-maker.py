175a3aeac74d92b7de2c0233d3fc45dcd5eca33f
010Ri <kouseisousei@gmail.com>
2022-08-17 00:52:00 +0900

    Update pamphlet-maker.py

--- pamphlet-maker.py
+++ pamphlet-maker.py
@@ -21,13 +21,12 @@ def add_page(file_name):
     # 画像の回転
     # pic.rotation = 20
 
-# layout-flame_1-Lとlayout-flame_1(8)-Lのテキストボックス調整用の関数
+# layout-flame_1-L , layout-flame_1(8)-Lのテキストボックス調整用の関数
 def modify_textbox_position_1_L(id):
-    # 1-Lのテキストボックス位置調整
     slide = pt.slides[id]  # layout-flame_something
     # テキストボックスを挿入
     textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
-    textbox.text = 'ここに会社名が入ります'
+    textbox.text = '広告(会社名)・イベント名'
     textbox.rotation = -90
     text_frame = textbox.text_frame
     text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
@@ -36,13 +35,12 @@ def modify_textbox_position_1_L(id):
     textbox.top = Cm(12.35)
     textbox.left = Cm(-2.73)
 
-# layout-flame_1-Rとlayout-flame_1(8)-Rのテキストボックス調整用の関数
+# layout-flame_1-R , layout-flame_1(8)-Rのテキストボックス調整用の関数
 def modify_textbox_position_1_R(id):
-    # 1-Lのテキストボックス位置調整
     slide = pt.slides[id]  # layout-flame_something
     # テキストボックスを挿入
     textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
-    textbox.text = 'ここに会社名が入ります'
+    textbox.text = '広告(会社名)・イベント名'
     textbox.rotation = 90
     text_frame = textbox.text_frame
     text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
@@ -53,36 +51,34 @@ def modify_textbox_position_1_R(id):
 
 # layout-flame_2-2-Lのテキストボックス調整用の関数
 def modify_textbox_position_2_2_L(id):
-    # 1-Lのテキストボックス位置調整
     slide = pt.slides[id]  # layout-flame_something
     # テキストボックスを挿入（上側）
     textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
-    textbox.text = 'ここに会社名が入ります'
+    textbox.text = '広告(会社名)・イベント名'
     textbox.rotation = -90
     text_frame = textbox.text_frame
     text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
     text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
     text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
     textbox.top = Cm(6.535)
-    textbox.left = Cm(-2.73)
+    textbox.left = Cm(-2.7)
     # テキストボックスを挿入（下側）
     textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
-    textbox.text = 'ここに会社名が入ります'
+    textbox.text = '広告(会社名)・イベント名'
     textbox.rotation = -90
     text_frame = textbox.text_frame
     text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
     text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
     text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
     textbox.top = Cm(18.195)
-    textbox.left = Cm(-2.73)
+    textbox.left = Cm(-2.7)
 
 # layout-flame_2-2-Rのテキストボックス調整用の関数
 def modify_textbox_position_2_2_R(id):
-    # 1-Lのテキストボックス位置調整
     slide = pt.slides[id]  # layout-flame_something
     # テキストボックスを挿入（上側）
     textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
-    textbox.text = 'ここに会社名が入ります'
+    textbox.text = '広告(会社名)・イベント名'
     textbox.rotation = 90
     text_frame = textbox.text_frame
     text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
@@ -92,7 +88,7 @@ def modify_textbox_position_2_2_R(id):
     textbox.left = Cm(10.95)
     # テキストボックスを挿入（下側）
     textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
-    textbox.text = 'ここに会社名が入ります'
+    textbox.text = '広告(会社名)・イベント名'
     textbox.rotation = 90
     text_frame = textbox.text_frame
     text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
@@ -101,6 +97,210 @@ def modify_textbox_position_2_2_R(id):
     textbox.top = Cm(18.195)
     textbox.left = Cm(10.95)
 
+# layout-flame_2-4-4-L , layout-flame_2-4-8-Lのテキストボックス調整用の関数
+def modify_textbox_position_2_4_L(id):
+    slide = pt.slides[id]  # layout-flame_something
+    # テキストボックスを挿入（上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告(会社名)・イベント名'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(6.535)
+    textbox.left = Cm(-2.65)
+    # テキストボックスを挿入（中側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(15.3)
+    textbox.left = Cm(-0.165)
+    # テキストボックスを挿入（下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(21.1)
+    textbox.left = Cm(-0.165)
+
+# layout-flame_2-4-4-R , layout-flame_2-4-8-Rのテキストボックス調整用の関数
+def modify_textbox_position_2_4_R(id):
+    slide = pt.slides[id]  # layout-flame_something
+    # テキストボックスを挿入（上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告(会社名)・イベント名'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(6.535)
+    textbox.left = Cm(10.95)
+    # テキストボックスを挿入（中側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(15.12)
+    textbox.left = Cm(13.45)
+    # テキストボックスを挿入（下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(20.92)
+    textbox.left = Cm(13.45)
+
+# layout-flame_3-4-L , layout-flame_3-8-L , layout-flame_3(4)-4-L , layout-flame_3(4)-8-L , layout-flame_3(8)-4-Lのテキストボックス調整用の関数
+def modify_textbox_position_3_L(id):
+    slide = pt.slides[id]  # layout-flame_something
+    # テキストボックスを挿入（上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告(会社名)・イベント名'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(9.55)
+    textbox.left = Cm(-2.7)
+    # テキストボックスを挿入（下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(21.1)
+    textbox.left = Cm(-0.165)
+
+# layout-flame_3-4-R , layout-flame_3-8-R , layout-flame_3(4)-4-R , layout-flame_3(4)-8-R , layout-flame_3(8)-8-Rのテキストボックス調整用の関数
+def modify_textbox_position_3_R(id):
+    slide = pt.slides[id]  # layout-flame_something
+    # テキストボックスを挿入（上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(10), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告(会社名)・イベント名'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(9.4)
+    textbox.left = Cm(10.95)
+    # テキストボックスを挿入（下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(21)
+    textbox.left = Cm(13.45)
+
+# layout-flame_4-4-4-4-L , layout-flame_4-4-4-8-Lのテキストボックス調整用の関数
+def modify_textbox_position_4_L(id):
+    slide = pt.slides[id]  # layout-flame_something
+    # テキストボックスを挿入（上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(3.8)
+    textbox.left = Cm(0.1)
+    # テキストボックスを挿入（中上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(9.567)
+    textbox.left = Cm(0.1)
+    # テキストボックスを挿入（中下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(15.433)
+    textbox.left = Cm(0.1)
+    # テキストボックスを挿入（下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = -90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(21.2)
+    textbox.left = Cm(0.1)
+
+# layout-flame_4-4-4-4-R , layout-flame_4-4-4-8-Rのテキストボックス調整用の関数
+def modify_textbox_position_4_R(id):
+    slide = pt.slides[id]  # layout-flame_something
+    # テキストボックスを挿入（上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(3.7)
+    textbox.left = Cm(13.08)
+    # テキストボックスを挿入（中上側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(9.52)
+    textbox.left = Cm(13.08)
+    # テキストボックスを挿入（中下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(15.25)
+    textbox.left = Cm(13.08)
+    # テキストボックスを挿入（下側）
+    textbox = slide.shapes.add_textbox(0, 0, Cm(5), Cm(1))  # (x座標, y座標, 横幅, 縦幅)
+    textbox.text = '広告'
+    textbox.rotation = 90
+    text_frame = textbox.text_frame
+    text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+    text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+    text_frame.autosize = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+    textbox.top = Cm(21.05)
+    textbox.left = Cm(13.08)
+    
 
 # Presentationクラスをインスタンス化
 pt = Presentation()
@@ -112,17 +312,15 @@ pt.slide_width=Cm(18.2)
 # 白紙のページを追加
 slide_layout = pt.slide_layouts[6]
 slide = pt.slides.add_slide(slide_layout)
-
 # テキストボックスを挿入
 textbox = slide.shapes.add_textbox(0, 0, Cm(15), Cm(5))  # (x座標, y座標, 横幅, 縦幅)
 text_frame = textbox.text_frame
 # text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
 # text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
-text_frame.text = 'テキストボックスを挿入しました。'
-
+text_frame.text = 'python-pptxでパワポを自動生成するやつ'
 # テキストボックスに段落の追加
 p = text_frame.add_paragraph()
-p.text = 'テキストボックスに段落を追加しました。'
+p.text = 'テキストボックスの位置は適当です'
 
 
 # file name list
@@ -179,21 +377,61 @@ for file_name in file_name_list:
     elif file_name == "layout-flame_2-2-R" : 
         id = 6  # layout-flame_2-2-R
         modify_textbox_position_2_2_R(id)
+    elif file_name == "layout-flame_2-4-4-L" : 
+        id = 7  # layout-flame_2-4-4-L
+        modify_textbox_position_2_4_L(id)
+    elif file_name == "layout-flame_2-4-4-R" : 
+        id = 8  # layout-flame_2-4-4-R
+        modify_textbox_position_2_4_R(id)
+    elif file_name == "layout-flame_2-4-8-L" : 
+        id = 9  # layout-flame_2-4-8-L
+        modify_textbox_position_2_4_L(id)
+    elif file_name == "layout-flame_2-4-8-R" : 
+        id = 10  # layout-flame_2-4-8-R
+        modify_textbox_position_2_4_R(id)
+    elif file_name == "layout-flame_3-4-L" : 
+        id = 11  # layout-flame_3-4-L
+        modify_textbox_position_3_L(id)
+    elif file_name == "layout-flame_3-4-R" : 
+        id = 12  # layout-flame_3-4-R
+        modify_textbox_position_3_R(id)
+    elif file_name == "layout-flame_3-8-L" : 
+        id = 13  # layout-flame_3-8-L
+        modify_textbox_position_3_L(id)
+    elif file_name == "layout-flame_3-8-R" : 
+        id = 14  # layout-flame_3-8-R
+        modify_textbox_position_3_R(id)
+    elif file_name == "layout-flame_3(4)-4-L" : 
+        id = 15  # layout-flame_3(4)-4-L
+        modify_textbox_position_3_L(id)
+    elif file_name == "layout-flame_3(4)-4-R" : 
+        id = 16  # layout-flame_3(4)-4-R
+        modify_textbox_position_3_R(id)
+    elif file_name == "layout-flame_3(4)-8-L" : 
+        id = 17  # layout-flame_3(4)-8-L
+        modify_textbox_position_3_L(id)
+    elif file_name == "layout-flame_3(4)-8-R" : 
+        id = 18  # layout-flame_3(4)-8-R
+        modify_textbox_position_3_R(id)
+    elif file_name == "layout-flame_3(8)-4-L" : 
+        id = 19  # layout-flame_3(8)-4-L
+        modify_textbox_position_3_L(id)
+    elif file_name == "layout-flame_3(8)-8-R" : 
+        id = 20  # layout-flame_3(8)-8-R
+        modify_textbox_position_3_R(id)
+    elif file_name == "layout-flame_4-4-4-4-L" : 
+        id = 21  # layout-flame_4-4-4-4-L
+        modify_textbox_position_4_L(id)
+    elif file_name == "layout-flame_4-4-4-4-R" : 
+        id = 22  # layout-flame_4-4-4-4-R
+        modify_textbox_position_4_R(id)
+    elif file_name == "layout-flame_4-4-4-8-L" : 
+        id = 23  # layout-flame_4-4-4-8-L
+        modify_textbox_position_4_L(id)
+    elif file_name == "layout-flame_4-4-4-8-R" : 
+        id = 24  # layout-flame_4-4-4-8-R
+        modify_textbox_position_4_R(id)
 
-
-"""
-# 図形の挿入
-rect = slide.shapes.add_shape(MSO_SHAPE.RECTANGLE, 0, 0, Cm(5), Cm(3)) 
-# 図形のスタイル設定
-rect.fill.solid() #塗りつぶし
-rect.fill.fore_color.rgb = RGBColor(0, 0, 255) #塗りつぶし色の指定
-rect.line.width = Mm(2) #枠線の指定、今回は2mm
-rect.line.color.rgb = RGBColor(255, 0, 0) #枠線の色指定
-# 図形にテキストを挿入
-pg = rect.text_frame.paragraphs[0] # 図形からtext_frameオブジェクトを取り出し、1つ目のパラグラフを取得
-pg.text = '図形です。'
-pg.font.size = Pt(20) # テキストサイズ、今回は20ポイント
-"""
 
 # ファイルを任意の名前で保存（現在時刻をファイル名として保存するようにしている）
 now = datetime.datetime.now()  # 現在時刻の取得
