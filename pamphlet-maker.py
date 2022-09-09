535ea8e251482e0b4e83a88ee2948675b7dcb57a
010Ri <kouseisousei@gmail.com>
2022-09-09 22:49:53 +0900

    update

--- pamphlet-maker.py
+++ pamphlet-maker.py
@@ -12,7 +12,6 @@ import glob
 
 
 # TODO: 画像を透過するプログラムを作る
-# TODO: 画像をpngで保存（画質はできるだけ高く）
 # TODO: 画像を挿入していい感じに配置する
 # MEMO: 画像の大きさは枠いっぱいにする？（アスペクト比変わっちゃうけど・・・）
 
@@ -309,12 +308,109 @@ def insert_textbox_4_R(id):
     textbox.top = Cm(21.05)
     textbox.left = Cm(13.08)
 
+
 # layout-flame_1-Lの画像調整用の関数
 def insert_ad_1_L(id, ad):
     slide = pt.slides[id]  # layout-flame_something
     # 画像の挿入
     file_path = ad
     slide.shapes.add_picture(file_path, Cm(2.97), Cm(1.54), Cm(13.61), Cm(22.63))
+
+# layout-flame_1-Rの画像調整用の関数
+def insert_ad_1_R(id, ad):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad
+    slide.shapes.add_picture(file_path, Cm(1.62), Cm(1.54), Cm(13.61), Cm(22.63))
+
+# layout-flame_1(8)-Lの画像調整用の関数
+def insert_ad_8_L(id, ad1, ad2, ad3, ad4, ad5, ad6, ad7, ad8):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad1
+    slide.shapes.add_picture(file_path, Cm(2.97), Cm(1.54), Cm(6.63), Cm(5.58))
+    file_path = ad2
+    slide.shapes.add_picture(file_path, Cm(2.97), Cm(7.32), Cm(6.63), Cm(5.58))
+    file_path = ad3
+    slide.shapes.add_picture(file_path, Cm(2.97), Cm(12.98), Cm(6.63), Cm(5.58))
+    file_path = ad4
+    slide.shapes.add_picture(file_path, Cm(2.97), Cm(18.56), Cm(6.63), Cm(5.58))
+    file_path = ad5
+    slide.shapes.add_picture(file_path, Cm(9.91), Cm(1.54), Cm(6.63), Cm(5.58))
+    file_path = ad6
+    slide.shapes.add_picture(file_path, Cm(9.91), Cm(7.32), Cm(6.63), Cm(5.58))
+    file_path = ad7
+    slide.shapes.add_picture(file_path, Cm(9.91), Cm(12.98), Cm(6.63), Cm(5.58))
+    file_path = ad8
+    slide.shapes.add_picture(file_path, Cm(9.91), Cm(18.56), Cm(6.63), Cm(5.58))
+
+# layout-flame_1(8)-Rの画像調整用の関数
+def insert_ad_8_R(id, ad1, ad2, ad3, ad4, ad5, ad6, ad7, ad8):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad1
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(1.54), Cm(6.63), Cm(5.58))
+    file_path = ad2
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(7.22), Cm(6.63), Cm(5.58))
+    file_path = ad3
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(12.85), Cm(6.63), Cm(5.58))
+    file_path = ad4
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(18.59), Cm(6.63), Cm(5.58))
+    file_path = ad5
+    slide.shapes.add_picture(file_path, Cm(8.49), Cm(1.54), Cm(6.63), Cm(5.58))
+    file_path = ad6
+    slide.shapes.add_picture(file_path, Cm(8.49), Cm(7.22), Cm(6.63), Cm(5.58))
+    file_path = ad7
+    slide.shapes.add_picture(file_path, Cm(8.49), Cm(12.85), Cm(6.63), Cm(5.58))
+    file_path = ad8
+    slide.shapes.add_picture(file_path, Cm(8.49), Cm(18.59), Cm(6.63), Cm(5.58))
+
+# layout-flame_2-2-Lの画像調整用の関数
+def insert_ad_2_2_L(id, ad1, ad2):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad1
+    slide.shapes.add_picture(file_path, Cm(2.97), Cm(1.54), Cm(13.56), Cm(11.01))
+    file_path = ad2
+    slide.shapes.add_picture(file_path, Cm(2.97), Cm(13.15), Cm(13.56), Cm(11.01))
+
+# layout-flame_2-2-Rの画像調整用の関数
+def insert_ad_2_2_R(id, ad1, ad2):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad1
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(1.54), Cm(13.56), Cm(11.01))
+    file_path = ad2
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(13.19), Cm(13.56), Cm(11.01))
+
+# layout-flame_2-4-4-Lの画像調整用の関数
+def insert_ad_2_4_4_L(id, ad1, ad2, ad3):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad1
+    slide.shapes.add_picture(file_path, Cm(3.09), Cm(1.54), Cm(13.56), Cm(11.01))
+    file_path = ad2
+    slide.shapes.add_picture(file_path, Cm(3.09), Cm(13.24), Cm(13.56), Cm(5.12))
+    file_path = ad3
+    slide.shapes.add_picture(file_path, Cm(3.09), Cm(19.04), Cm(13.56), Cm(5.12))
+
+# layout-flame_2-4-4-Rの画像調整用の関数
+def insert_ad_2_4_4_R(id, ad1, ad2, ad3):
+    slide = pt.slides[id]  # layout-flame_something
+    # 画像の挿入
+    file_path = ad1
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(1.45), Cm(13.56), Cm(11.01))
+    file_path = ad2
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(13.06), Cm(13.56), Cm(5.12))
+    file_path = ad3
+    slide.shapes.add_picture(file_path, Cm(1.68), Cm(18.86), Cm(13.56), Cm(5.12))
+
+
+
+
+
+
+
 
 # Presentationクラスをインスタンス化
 pt = Presentation()
@@ -375,7 +471,7 @@ for layout_flame in layout_flame_list:
     # テキストボックス調整
     if layout_flame == "layout-flame_1-L" :
         id = 1  # layout-flame_1-L
-        ads = glob.glob('./img/ad-img/*.1_*.png')  # xxx.1_xxx.pngという名前のファイルをさがす
+        ads = glob.glob('./img/ad-img/*.1_*.*')  # xxx.1_xxx.pngという名前のファイルをさがす
         # print(len(ads))
         length = len(ads)
         ad = ads[length-length+1]  # 添字により任意の写真を指定
@@ -384,25 +480,77 @@ for layout_flame in layout_flame_list:
         insert_ad_1_L(id, ad)
     elif layout_flame == "layout-flame_1-R" :
         id = 2  # layout-flame_1-R
+        ads = glob.glob('./img/ad-img/*.1_*.*') 
+        length = len(ads)
+        ad = ads[length-length+1]
         insert_textbox_1_R(id)
+        insert_ad_1_R(id, ad)
     elif layout_flame == "layout-flame_1(8)-L" : 
         id = 3  # layout-flame_1(8)-L
+        ads = glob.glob('./img/ad-img/*.8_*.*') 
+        length = len(ads)
+        ad1 = ads[length-length]
+        ad2 = ads[length-length]
+        ad3 = ads[length-length]
+        ad4 = ads[length-length]
+        ad5 = ads[length-length]
+        ad6 = ads[length-length]
+        ad7 = ads[length-length]
+        ad8 = ads[length-length]
         insert_textbox_1_L(id)
+        insert_ad_8_L(id, ad1, ad2, ad3, ad4, ad5, ad6, ad7, ad8)
     elif layout_flame == "layout-flame_1(8)-R" : 
         id = 4  # layout-flame_1(8)-R
+        ads = glob.glob('./img/ad-img/*.8_*.*') 
+        length = len(ads)
+        ad1 = ads[length-length]
+        ad2 = ads[length-length]
+        ad3 = ads[length-length]
+        ad4 = ads[length-length]
+        ad5 = ads[length-length]
+        ad6 = ads[length-length]
+        ad7 = ads[length-length]
+        ad8 = ads[length-length]
         insert_textbox_1_R(id)
+        insert_ad_8_R(id, ad1, ad2, ad3, ad4, ad5, ad6, ad7, ad8)
     elif layout_flame == "layout-flame_2-2-L" : 
         id = 5  # layout-flame_2-2-L
+        ads = glob.glob('./img/ad-img/*.2_*.*') 
+        length = len(ads)
+        ad1 = ads[length-length]
+        ad2 = ads[length-length]
         insert_textbox_2_2_L(id)
+        insert_ad_2_2_L(id, ad1, ad2)
     elif layout_flame == "layout-flame_2-2-R" : 
         id = 6  # layout-flame_2-2-R
+        ads = glob.glob('./img/ad-img/*.2_*.*') 
+        length = len(ads)
+        ad1 = ads[length-length]
+        ad2 = ads[length-length]
         insert_textbox_2_2_R(id)
+        insert_ad_2_2_R(id, ad1, ad2)
     elif layout_flame == "layout-flame_2-4-4-L" : 
         id = 7  # layout-flame_2-4-4-L
+        ads = glob.glob('./img/ad-img/*.2_*.*') 
+        length = len(ads)
+        ad1 = ads[length-length]
+        ads = glob.glob('./img/ad-img/*.4_*.*') 
+        length = len(ads)
+        ad2 = ads[length-length]
+        ad3 = ads[length-length]
         insert_textbox_2_4_L(id)
+        insert_ad_2_4_4_L(id, ad1, ad2, ad3)
     elif layout_flame == "layout-flame_2-4-4-R" : 
         id = 8  # layout-flame_2-4-4-R
+        ads = glob.glob('./img/ad-img/*.2_*.*') 
+        length = len(ads)
+        ad1 = ads[length-length]
+        ads = glob.glob('./img/ad-img/*.4_*.*') 
+        length = len(ads)
+        ad2 = ads[length-length]
+        ad3 = ads[length-length]
         insert_textbox_2_4_R(id)
+        insert_ad_2_4_4_R(id, ad1, ad2, ad3)
     elif layout_flame == "layout-flame_2-4-8-L" : 
         id = 9  # layout-flame_2-4-8-L
         insert_textbox_2_4_L(id)
